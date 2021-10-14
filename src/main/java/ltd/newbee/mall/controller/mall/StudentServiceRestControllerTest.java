d876ff16ff749d2572c946c6f21449ce2f3a34f6
WUJUNJIE <clearlove008@gmail.com>
2021-10-14 19:17:32 +0900

    add update,insert and delete

--- src/main/java/ltd/newbee/mall/controller/mall/StudentServiceRestControllerTest.java
+++ src/main/java/ltd/newbee/mall/controller/mall/StudentServiceRestControllerTest.java
@@ -7,12 +7,16 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import ltd.newbee.mall.common.Constants;
+import ltd.newbee.mall.common.ServiceResultEnum;
 import ltd.newbee.mall.controller.vo.NewBeeMallIndexCategoryVO;
 import ltd.newbee.mall.entity.Student;
 import ltd.newbee.mall.service.StudentService;
@@ -26,9 +30,9 @@ public class StudentServiceRestControllerTest {
     private StudentService studentService;
 	
     //@CrossOrigin(origins = "http://localhost:8081")
-    @RequestMapping(value = "/student", method = RequestMethod.POST)
+    @RequestMapping(value = "/students", method = RequestMethod.GET)
     @ResponseBody
-    public Result categories(@RequestBody String name) {
+    public Result selectByName(@RequestBody String name) {
     	
     	List<Student> list = studentService.selectByName(name);
         if (CollectionUtils.isEmpty(list)) {
@@ -36,7 +40,42 @@ public class StudentServiceRestControllerTest {
         }else {
         	return ResultGenerator.genSuccessResult(list);
         }
-    
     }
 
+    @RequestMapping(value = "/insertStudent", method = RequestMethod.POST)
+    @ResponseBody
+    public Result insertStudent(@RequestBody Student s) {
+    	
+    	int count = studentService.insertStudent(s);
+        if (count<=0) {
+        	return ResultGenerator.genErrorResult(Constants.CATEGORY_FETCH_ERROR, Constants.CATEGORY_FETCH_ERROR_MESSAGE);
+        }else {
+        	return ResultGenerator.genSuccessResult("挿入できました");
+        }
+    }
+
+    @PutMapping(value = "/updateStudent")
+    @ResponseBody
+    public Result updateStudent(@RequestBody Student s) {
+    	
+    	String count = studentService.updateStudent(s);
+        if (count.equals(ServiceResultEnum.SUCCESS.getResult())) {
+        	return ResultGenerator.genSuccessResult("更新できました");
+        }else {
+        	return ResultGenerator.genErrorResult(Constants.CATEGORY_FETCH_ERROR, Constants.CATEGORY_FETCH_ERROR_MESSAGE);
+        }
+    }
+    
+    @DeleteMapping(value = "/deleteStudent/{studentID}")
+    @ResponseBody
+    public Result deleteStudent(@PathVariable("studentID") int studentID) {
+    	
+    	boolean count = studentService.deleteStudentByStudentID(studentID);
+        if (count) {
+        	return ResultGenerator.genSuccessResult("削除しました");
+        }else {
+        	return ResultGenerator.genErrorResult(Constants.CATEGORY_FETCH_ERROR, Constants.CATEGORY_FETCH_ERROR_MESSAGE);
+        }
+    }
+    
 }
--- src/main/java/ltd/newbee/mall/service/StudentService.java
+++ src/main/java/ltd/newbee/mall/service/StudentService.java
@@ -9,4 +9,12 @@ public interface StudentService {
 
 	ArrayList<Student> selectByName(String name);
 	
+	int insertStudent(Student s);
+	
+//	int selectByStudentID(int studentID);
+	
+	String updateStudent(Student s);
+	
+	boolean deleteStudentByStudentID(int studentID);
+	
 }
